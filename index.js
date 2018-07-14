fb124e43a6c02fa8a81ee4eeba4baa31822308ae
Suchitra Naik <susanaik@gmail.com>
2018-07-14 22:08:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ class Customer{
     store.customers.push(this)
   }
   deliveries(){
-    return store.deliveries().filter(delivery => delivery.customer());
+    return store.deliveries.filter(delivery => delivery.customer());
   }
 
   meals() {
